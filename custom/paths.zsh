208cf2cf1338ad73c738ca3ec7a36051b825c355
ecoutu <ecoutu@viralnation.com>
2026-02-14 13:56:05 -0700

    feat(custom): :recycle: use custom code feature
    Signed-off-by: ecoutu <ecoutu@viralnation.com>

---
+++ custom/paths.zsh
@@ -0,0 +1,36 @@
+# Set the the list of directories that cd searches.
+cdpath+=(
+  ${HOME}
+  ${DEV_ROOT}
+  ${ECO_ROOT}
+  ${ECO_SRC}
+  ${EMPLOYER_ROOT}
+  ${EMPLOYER_SRC}
+)
+
+# Set the list of directories that Zsh searches for programs.
+path+=(
+  ${HOME}/.local/{,s}bin
+  ${HOME}/{,s}bin(N)
+
+  /usr/local/{,s}bin(N)
+)
+
+case "$OSTYPE" in
+  darwin*)
+    path+=(
+      /opt/{homebrew,local}/{,s}bin(N)
+    )
+    ;;
+esac
+
+PROJECT_PATHS=(
+  "${ECO_SRC}"
+  "${EMPLOYER_SRC}"
+)
+
+fpath+=(
+  ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
+  "${HOME}/.zsh/completions"
+  "${HOME}/.local/share/mise/installs/zoxide/latest/completions"
+)
